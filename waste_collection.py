f983d98d161c0d4f15e40cd0ce2a991514c77a00
grro <gregor.roth@1und1.de>
2024-10-20 17:52:18 +0200

    reload period decreased

--- waste_collection.py
+++ waste_collection.py
@@ -140,6 +140,6 @@ class WasteCollectionSchedule:
         while self.__is_running:
             try:
                 self.__reload()
-                sleep(19)
+                sleep(60)
             except Exception as e:
                 logging.warning("error occurred on sync " + str(e))
